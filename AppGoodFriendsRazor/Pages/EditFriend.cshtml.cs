e48b5be8f1754d7d350ed08f877f1e787329c05a
zeleron7 <jessie.martensson@hotmail.com>
2025-01-07 14:24:13 +0100

    1

--- AppGoodFriendsRazor/Pages/EditFriend.cshtml.cs
+++ AppGoodFriendsRazor/Pages/EditFriend.cshtml.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using DbModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,6 +13,7 @@ namespace MyApp.Namespace
          readonly IFriendsService _service;
 
         //friend
+
         public IFriend Friend { get; set; }
 
         public async Task <IActionResult> OnGet(Guid friendId)
@@ -23,8 +25,42 @@ namespace MyApp.Namespace
 
         public async Task<IActionResult> OnPostEdit(Guid friendId, string FirstName, string LastName, string Email, DateTime Birthday, string Address, int ZipCode, string City, string Country)
         {
-            
-            if (friendId != Guid.Parse("00000000-0000-0000-0000-000000000000")) 
+            try
+            {
+                if (friendId != Guid.Parse("00000000-0000-0000-0000-000000000000")) 
+                {
+                    var friend = await _service.ReadFriendAsync(friendId, false);
+
+                    if (friend != null) 
+                    {
+                        var friendCUdto = new FriendCUdto(friend);
+
+                        var address = await _service.ReadAddressAsync((Guid)friendCUdto.AddressId, false);
+
+                        friendCUdto.FirstName = FirstName;
+                        friendCUdto.LastName = LastName;
+                        friendCUdto.Email = Email;
+                        friendCUdto.Birthday = Birthday;
+
+                        var addressCUdto = new AddressCUdto(address);
+
+                        addressCUdto.StreetAddress = Address;
+                        addressCUdto.City = City;
+                        addressCUdto.Country = Country;
+                        addressCUdto.ZipCode = ZipCode;
+
+                        await _service.UpdateFriendAsync(friendCUdto);
+                        await _service.UpdateAddressAsync(addressCUdto);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            /*if (friendId != Guid.Parse("00000000-0000-0000-0000-000000000000")) 
             {
                 var friend = await _service.ReadFriendAsync(friendId, false);
 
@@ -49,7 +85,7 @@ namespace MyApp.Namespace
                     await _service.UpdateFriendAsync(friendCUdto);
                     await _service.UpdateAddressAsync(addressCUdto);
                 }
-            }
+            }*/
         
 
             return await OnGet(friendId);
@@ -61,3 +97,5 @@ namespace MyApp.Namespace
         }
     }
 }
+
+
